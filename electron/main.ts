b95c9f12c546eb5da3199a9097156eda5264eb34
CJ Amanambu <oamanambu@yahoo.com>
2019-11-26 20:32:51 +0100

    Refactored electron entry point to use static class. This was done in order to reduce dependencies and enable easier testing.

--- electron/main.ts
+++ electron/main.ts
@@ -1,39 +1,51 @@
-import { BrowserWindow, app } from 'electron';
-import * as path from 'path';
+import { BrowserWindow } from 'electron';
 import * as url from 'url';
+import * as path from 'path';
 
-let mainWindow: BrowserWindow;
+export default class Main {
+    static mainWindow: Electron.BrowserWindow;
+    static application: Electron.App;
+    static BrowserWindow;
 
-function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800, height: 600
-    });
+    private static onWindowAllClosed() {
+        if (process.platform !== 'darwin') {
+            Main.application.quit();
+        }
+    }
 
-    mainWindow.loadURL(
-        url.format({
-            pathname: path.join(__dirname, `/../../dist/index.html`),
-            protocol: 'file:',
-            slashes: true
-        })
-    );
+    private static onClosed() {
+        Main.mainWindow = null;
+    }
 
-    mainWindow.webContents.openDevTools();
+    private static onReadyToShow() {
+        Main.mainWindow.show();
+    }
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+    private static onReady() {
+        Main.mainWindow = new Main.BrowserWindow({
+            width: 800, height: 600,
+            show: false,
+            webPreferences: {
+                nodeIntegration: true
+            }
+        });
+
+        Main.mainWindow.loadURL(
+            url.format({
+                pathname: path.join(__dirname, `/../../dist/index.html`),
+                protocol: 'file:',
+                slashes: true
+            })
+        );
+
+        Main.mainWindow.once('ready-to-show', Main.onReadyToShow)
+        Main.mainWindow.on('closed', Main.onClosed);
+    }
+
+    public static main(app: Electron.App, browserWindow: typeof BrowserWindow) {
+        Main.BrowserWindow = browserWindow;
+        Main.application = app;
+        Main.application.on('window-all-closed', Main.onWindowAllClosed);
+        Main.application.on('ready', Main.onReady);
+    }
 }
-
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
