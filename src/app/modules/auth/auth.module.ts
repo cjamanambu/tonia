7d664e5b6eee5fd9a32bb3039c3f2e1fa2764731
CJ Amanambu <oamanambu@yahoo.com>
2020-01-14 00:24:32 +0100

    Currently working on login page component

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,9 +1,20 @@
+// @angular
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// material
+import {
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule
+} from '@angular/material';
+
+// components
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
-
 
 @NgModule({
   declarations: [
@@ -11,7 +22,14 @@ import { RegisterComponent } from './register/register.component';
     RegisterComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+    MatButtonModule
   ],
   exports: [
     LoginComponent,
